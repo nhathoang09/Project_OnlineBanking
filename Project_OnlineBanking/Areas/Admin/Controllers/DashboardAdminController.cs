373ad15645b33959f74970649513adc566b62215
nhathoang09 <lenhath6@gmail.com>
2024-04-02 01:38:32 +0700

    Layout contact & admin

---
+++ Project_OnlineBanking/Areas/Admin/Controllers/DashboardAdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BootSTrapShop.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/dashboard")]
+    public class DashboardAdminController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project_OnlineBanking/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_OnlineBanking.Controllers
+{
+    [Route("contact")]
+    public class ContactController : Controller
+    {
+        [Route("index")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
